5af112b57066a2392edb1697ff9124c004f1416e
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-17 22:57:04 +0600

    added delete method

--- index.js
+++ index.js
@@ -64,6 +64,13 @@ async function run() {
     })
 
 
+    app.delete('/tshirts/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await tshirtCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
